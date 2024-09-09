c978083eaf8dd72faf1169885098e7ccbe05d445
dalmendras <dalmendras@almonlabs.com>
2024-09-09 15:25:15 -0300

    Cambio

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hola Mi Taller Push desde VSC
+          Hola Mi Taller Push desde VSC Cambio
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
